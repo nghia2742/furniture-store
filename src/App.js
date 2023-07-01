5723f531da93747ea1949dac3fe93fca3560a689
Trong Nghia <nghia.nt2704@gmail.com>
2023-07-01 15:55:20 +0700

    test fix deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { publicRoutes } from './routes';
+// import { publicRoutes } from './routes';
+import Home from './components/Home';
+import Shop from './components/Shop';
 
 function App() {
     return (
         <Router basename="/furniture-store">
             <div className="App">
                 <Routes>
-                    {publicRoutes.map((route, index) => {
+                    {/* {publicRoutes.map((route, index) => {
                         const Page = route.component;
                         return (
                             <Route key={index}
@@ -17,9 +19,10 @@ function App() {
                                 }
                             />
                         );
-                    })}
+                    })} */}
+                    <Route exact path="/" element={<Home />}></Route>
+                    <Route path="/shop" element={<Shop />}></Route>
                 </Routes>
-
             </div>
         </Router>
     );
