d0fe3126ed2ca522e9082c1eea77b4265d67da6e
Trong Nghia <nghia.nt2704@gmail.com>
2023-07-01 14:03:07 +0700

    Fix Deploy

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import {
+    createHashRouter,
+    RouterProvider
+  } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createHashRouter([
+    {
+      path: "/*",
+      element: <App />,
+    }
+  ]);
+  
+  ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <HashRouter>
-            <App />
-        </HashRouter>
+      <RouterProvider router={router} />
     </React.StrictMode>
-);
+  );
