340715826c9ac07f7b5828edf2cc619edc2ff54f
Trong Nghia <nghia.nt2704@gmail.com>
2023-07-01 14:14:51 +0700

    Fix deploy web

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from './routes';
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {
-    createHashRouter,
-    RouterProvider
-  } from 'react-router-dom';
 
-const router = createHashRouter([
-    {
-      path: "/*",
-      element: <App />,
-    }
-  ]);
-  
-  ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  );
+        <App />
+    </React.StrictMode>,
+);
