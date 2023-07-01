97ca926dfc2c12ef6c83ee3d0bc221415c0b7723
Trong Nghia <nghia.nt2704@gmail.com>
2023-07-01 15:22:56 +0700

    change Switch import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Switch } from 'react-router';
+
 import { publicRoutes } from './routes';
 
 function App() {
